0f68832f7d061cfac780e2b2c3bde92674f00b2d
Adzka sfr <94780705+adzka-sfr@users.noreply.github.com>
2025-06-06 08:38:44 +0700

    buka untuk server

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-// require_once $_SERVER['DOCUMENT_ROOT'] . '/config/connect.php'; // hosting
-require_once $_SERVER['DOCUMENT_ROOT'] . '/split_bill/config.php'; // local
-echo 'anjay';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php'; // hosting
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/split_bill/config.php'; // local
 if (isset($_SESSION['sb_id'])) {
     echo "<script>window.location='" . base_url('dashboard') . "';</script>";
 } else {
